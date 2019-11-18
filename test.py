44dc238edf3ce00775ceb49c35d3bb1707b955d2
Sandipan Kalita <b16cs143@cit.ac.in>
2019-11-18 14:22:07 +0530

    upload to clg server

--- test.py
+++ test.py
@@ -38,16 +38,16 @@ encodings = []
 names = []
 
 # Training directory
-train_dir = os.listdir('/train_dir/')
+train_dir = os.listdir('train_dir/')
 
 # Loop through each person in the training directory
 for person in train_dir:
-    pix = os.listdir("/train_dir/" + person)
+    pix = os.listdir("train_dir/" + person)
 
     # Loop through each training image for the current person
     for person_img in pix:
         # Get the face encodings for the face in each image file
-        face = face_recognition.load_image_file("/train_dir/" + person + "/" + person_img)
+        face = face_recognition.load_image_file("train_dir/" + person + "/" + person_img)
         face_bounding_boxes = face_recognition.face_locations(face)
 
         #If training image contains none or more than faces, print an error message and exit
